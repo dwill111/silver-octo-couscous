de9190088b88997939129eb3816ec6c18c5fdea7
dwill111 <dwill111@emich.edu>
2016-11-29 11:56:24 -0500

    Add files via upload
    More Completed Version the only thing that is missing is the removeAll()

--- test2.java
+++ test2.java
@@ -5,25 +5,19 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
-import java.awt.*;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class test2 extends JFrame implements ActionListener {
-	private JPanel panel;
+	private static JPanel panel;
+	
 
 	public static void main(String[] args) {
-		LinkedList<JButton> list = new LinkedList<JButton>();
-		String fileName1 = "file1.txt";
-		String fileName2 = "file2.txt";
-		String fileName3 = "file3.txt";
-		Scanner keyIn = new Scanner(System.in);
 
-		test2 method = new test2();
 		JFrame window = new JFrame();
 		window.setSize(500, 500);
 		window.setVisible(true);
@@ -31,23 +25,18 @@ public class test2 extends JFrame implements ActionListener {
 		BorderLayout layout = new BorderLayout();
 		JButton button1 = new JButton("Load file");
 		JButton exitButton = new JButton("Click to exit");
-		
+
 		test2 buttonear = new test2();
 		button1.addActionListener(buttonear);
 		exitButton.addActionListener(buttonear);
 
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		JPanel panel1 = new JPanel();
 		panel1.add(button1);
 		panel1.add(exitButton);
-		
+
 		window.add(panel1, layout.NORTH);
 		window.add(panel, FlowLayout.CENTER);
-		
-//		System.out.println("Enter a number: ");
-//		int number = keyIn.nextInt();
-//
-//		System.out.println(method.recursiveInteger(number));
 	}
 
 	public int recursiveInteger(int n) {
@@ -66,11 +55,35 @@ public class test2 extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		String buttonText = event.getActionCommand();
 		JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+	
+		LinkedList<JButton> list = new LinkedList<JButton>();
+
 		if (buttonText.equals("Load file")) {
 			fileChooser.showOpenDialog(panel);
+
+			Scanner keyIn;
+			try {
+				keyIn = new Scanner(fileChooser.getSelectedFile());
+				while (keyIn.hasNextInt()) {
+					int result = recursiveInteger(keyIn.nextInt());
+					JButton button = new JButton("" + result);
+					list.add(button);
+					for (int i = 0; i < list.size(); i++) {
+						panel.add(list.get(i));
+						if (button.equals(i)) {
+							panel.removeAll();
+						}
+
+					}
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
 		}
-		
-		else if(buttonText.equals("Click to exit")){
+
+		else if (buttonText.equals("Click to exit")) {
 			System.exit(0);
 		}
 
